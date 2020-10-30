2d60308cbaa312b391817ec19f0b5d5cc1c7e9a5
Andrew Erskine <erskine94@gmail.com>
2020-10-29 21:11:20 -0700

    Testing editor scripting for stop-motioning animations

---
+++ Assets/AnimSandbox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AnimSandbox : ScriptableWizard
+{
+    public AnimationClip animationClip;
+
+    public string clipName;
+
+    [MenuItem("GameObject/AnimSandbox")]
+    static void CreateWizard() {
+        ScriptableWizard.DisplayWizard<AnimSandbox>("");
+    }
+
+    void OnWizardCreate() {
+        Debug.Log(animationClip);  
+        EditorCurveBinding[] curveBindings = AnimationUtility.GetCurveBindings(animationClip);
+        foreach (EditorCurveBinding curve in curveBindings) {
+            Debug.Log(curve.path);
+        }
+    }
+}
